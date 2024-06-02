0d50f869aa07a5d1f247f340c928d3e0fa6d951f
Ela <micaelafrancisco13@outlook.com>
2024-06-02 16:33:14 +0800

    implement fetching data

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+// queryClient - core object used by react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+          <App />
+      </QueryClientProvider>
   </React.StrictMode>
 );
--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
 
 interface Todo {
   id: number;
@@ -9,21 +10,52 @@ interface Todo {
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
+  // queryKey
+  // - unique ID for the query that's used internally for caching
+  // - the cached data is accessible via this key
+  // - it is set to an array with one or more values
+  //    first value is often a string that identifies the type of data we want to store here (e.g., 'todos')
+  //    second value is either a string or an object (e.g., 'completed', { completed: true }) for storing completed to-dos
 
-  useEffect(() => {
+  // queryFn
+  // - it is a function that is used to fetch data from the backend
+  // - it returns a promise that resolves to data, otherwise, it returns an error
+  // - the body of this function uses the function that gets the data from the backend (e.g., useData hook or axios.get())
+
+  const fetchTodos = () =>
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+        .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+        .then((res) => res.data)
+        .catch((error) => console.error(error));
 
-  if (error) return <p>{error}</p>;
+ const query = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodos
+  });
+ const { data: todos } = query;
+
+  // state hooks no longer needed
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [error, setError] = useState('');
+
+  // as well as the effect hook
+  // useEffect(() => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => setTodos(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
+
+  // if (error) return <p>{error}</p>;
+
+  // benefits earned:
+  // - automatic retries
+  // - automatic refresh
+  // - caching
 
   return (
     <ul className="list-group">
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <li key={todo.id} className="list-group-item">
           {todo.title}
         </li>
