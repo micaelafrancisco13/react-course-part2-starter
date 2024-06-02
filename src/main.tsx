aec27371c2bc00bff8172cbf3b4c84293b79928b
Ela <micaelafrancisco13@outlook.com>
2024-06-02 17:10:10 +0800

    add comments for using react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+// install npm i @tanstack/react-query-devtools
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +15,10 @@ ReactDOM.createRoot(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <App/>
+            <ReactQueryDevtools/>
         </QueryClientProvider>
     </React.StrictMode>
 );
+
+// react-query-devtools
+// # of observers is the # of components that are using a specific query
