bd344c3308bb363e483772876d812dd59e8222eb
Ela <micaelafrancisco13@outlook.com>
2024-06-04 10:33:24 +0800

    implement handling errors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -6,54 +6,55 @@ import axios from "axios";
 const TodoForm = () => {
     const queryClient = useQueryClient();
 
-    const addTodo = useMutation({
+    // if unknown type is encountered, deal with it using generics
+    // useMutation<DataTypeReceived, ErrorType, DataTypeSent>
+    const addTodo = useMutation<Todo, Error, Todo>({
         mutationFn: (todo: Todo) =>
-            axios.post<Todo>('https://jsonplaceholder.typicode.com/todos ', todo)
+            axios.post<Todo>('https://jsonplaceholder.typicode.com/todosx', todo)
                 .then(res => res.data),
         onSuccess: (savedTodo, newTodo) => {
-            console.log(savedTodo); // data from the server
-            console.log(newTodo); // object we sent to the server
+            console.log(savedTodo);
+            console.log(newTodo);
 
-            // we have 2 options for updating the list of to-dos:
-            // 1. invalidating the cache
-            //  - this re-fetches all the data from the BE
             queryClient.invalidateQueries({
                 queryKey: ['todos'],
             }).then(response => {
                 console.log(response)
             });
 
-            // 2. updating the data in the cache directly
-            //  - updates data in an immutable way
             queryClient.setQueryData<Todo[]>(['todos'], (todos) =>
                 [savedTodo, ...(todos || [])]);
         },
         // onError: () =>
-        // onSettled() gets called whether the request is successful or not
     });
     const ref = useRef<HTMLInputElement>(null);
 
     return (
-        <form className="row mb-3" onSubmit={event => {
-            event.preventDefault();
-            // mutation hook in react query
-            if (ref.current && ref.current.value) {
-                const newTodo = {
-                    id: 0,
-                    title: ref.current.value,
-                    completed: false,
-                    userId: 1
-                };
-                addTodo.mutate(newTodo);
-            }
-        }}>
-            <div className="col">
-                <input ref={ref} type="text" className="form-control"/>
-            </div>
-            <div className="col">
-                <button className="btn btn-primary">Add</button>
-            </div>
-        </form>
+        <>
+            {addTodo.error && (
+                <div className="alert alert-danger">{addTodo.error.message}</div>
+            )}
+            <form className="row mb-3" onSubmit={event => {
+                event.preventDefault();
+                // mutation hook in react query
+                if (ref.current && ref.current.value) {
+                    const newTodo = {
+                        id: 0,
+                        title: ref.current.value,
+                        completed: false,
+                        userId: 1
+                    };
+                    addTodo.mutate(newTodo);
+                }
+            }}>
+                <div className="col">
+                    <input ref={ref} type="text" className="form-control"/>
+                </div>
+                <div className="col">
+                    <button className="btn btn-primary">Add</button>
+                </div>
+            </form>
+        </>
     );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+const port = 5173;
+const open = `http://localhost:${port}`;
+
 export default defineConfig({
     plugins: [react()],
+    server: { open }
 })
