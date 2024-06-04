8a7bbfe533478152676b4ad8a99c89e5b32121a1
Ela <micaelafrancisco13@outlook.com>
2024-06-04 12:39:49 +0800

    implement optimistic update

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,20 +3,48 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Todo } from "../hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+    previousTodos: Todo[];
+}
+
 const TodoForm = () => {
     const queryClient = useQueryClient();
 
-    const addTodo = useMutation<Todo, Error, Todo>({
+    // when creating the mutation, we should provide a generic type argument
+    // for our context object <AddTodoContext>
+    const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
         mutationFn: (todo: Todo) =>
             axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
                 .then(res => res.data),
-        onSuccess: (savedTodo, newTodo) => {
+        // onMutate is called before the mutation is executed
+        // variables in React Query refer to the data we sent to the backend
+        onMutate: (newTodo: Todo) => {
+            // we should create a context and return it from this callback, so we can access it on
+            // onError() callback
+            const previousTodos = queryClient.getQueryData<Todo[]>(['todos']) || [];
+
+            // optimistic adding of todo
             queryClient.setQueryData<Todo[]>(['todos'], (todos) =>
-                [savedTodo, ...(todos || [])]);
+                [newTodo, ...(todos || [])]);
 
             if (ref.current)
                 ref.current.value = '';
+
+            return { previousTodos };
         },
+        onSuccess: (savedTodo, newTodo) => {
+            // update cached todo with the ID from the backend
+            queryClient.setQueryData<Todo[]>(['todos'], (todos) =>
+                todos?.map(todo => todo === newTodo ? savedTodo : todo));
+        },
+        // - the context parameter is the object we create to pass data in-between our callbacks
+        // - here, we need a context object that includes todos before we updated the cache (go to onMutate)
+        onError: (error, newTodo, context) => {
+            if (!context) return;
+
+            // rollback the UI from its previous state
+            queryClient.setQueryData<Todo[]>(['todos'], context.previousTodos);
+        }
     });
     const ref = useRef<HTMLInputElement>(null);
 
