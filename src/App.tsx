11c1007f52635856a494413cc7d7e206f0ba29ca
Ela <micaelafrancisco13@outlook.com>
2024-06-16 18:58:35 +0800

    create complex actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css';
 import TodoList from "./react-query/TodoList";
 import TodoForm from "./react-query/TodoForm";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
     return (
         <>
-            <Counter/>
+            <TaskList/>
         </>);
 }
 
